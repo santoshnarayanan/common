31a542f371b3f1b54fe53f91206d038fef4d1f79
Santosh Narayanan <santosh33@gmail.com>
2022-11-08 16:14:14 +0100

    Test for easy publish command

--- build/index.js
+++ build/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const color = {
-    red: 10, blue: 10, green: 10
+    red: 20, blue: 10, green: 10
 };
 console.log(color);
 exports.default = color;
